a67497b32a6ff0ef40caef4dc44d6bf14f8c7b5f
wulandari3082023-wq <wulandari3082023@gmail.com>
2026-05-06 19:57:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    alert("Terima kasih! Silakan hubungi WhatsApp untuk pembelian.");
+  });
+});document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    alert("Terima kasih! Silakan hubungi WhatsApp untuk pembelian.");
+  });
+});
